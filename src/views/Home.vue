62a684aa372c75c6ba95d4937fc203910347b338
Leo Bozkir <L.B.Bozkir@student.tudelft.nl>
2026-01-30 22:19:17 +0100

    added unified types for card preview + emits to hide other cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { Ref } from "vue";
+
 import GamePreview from '../components/GamePreview.vue';
+import _gamePreviewsJson from '@/static/gamePreviews.json'
+import type { IGamePreview } from "@/commons/types.ts"
+
+// globals
+const previews: IGamePreview[] = _gamePreviewsJson as IGamePreview[];
+
+// refs
+const openCardIndex: Ref<number | null> = ref(null);
+
+// functions
+function onSetOpen(new_id: number) {
+    openCardIndex.value = new_id
+}
+
 </script>
 
 <template>
-        <GamePreview 
-        title="Werewolves"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore "
-    />
-    <GamePreview 
-        title="30 seconds"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore"
-    />
-    <GamePreview 
-        title="Chameleon"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore "
+    <GamePreview
+        v-for="(preview, index) in previews"
+        :key="index"
+        v-bind="preview"
+        :openCardIndex="openCardIndex"
+        @set-open="onSetOpen"
     />
 </template>
 
