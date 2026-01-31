b59e6e886459dd8b64c648efc16d3d771ae73ed0
Leo Bozkir <L.B.Bozkir@student.tudelft.nl>
2026-01-31 01:40:35 +0100

    lol

--- src/components/GamePreview.vue
+++ src/components/GamePreview.vue
@@ -44,13 +44,18 @@ function toggleOpen() {
 <style scoped lang="scss">
 .card {
     margin: 10px;
-    transition: 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275), background-color 0s;
+
+    // transition: 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275), background-color 0s;
+    transition:
+        background-color 0s,
+        transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
     
     &.open {
+        transform: scaleY(10);
         display: flex;
         flex-direction: column;
         width: 300px;
-        height: 400px;
+        // height: 400px;
         // background: #F8F8F8;
         // background: lime
         border: 2px solid #000000;
