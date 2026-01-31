ea8e3e4bd64f27390a9f88a82885e6b3cebba9fd
kleinbroodje <nathantse2007@icloud.com>
2026-01-31 01:22:34 +0100

    added routing for games

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue"
+import Mystories from "@/views/Mystories.vue";
+import Werewolves from "@/views/Werewolves.vue";
+import HalfAMinute from "@/views/HalfAMinute.vue";
+import Chameleon from "@/views/Chameleon.vue";
 
 const routes = [
     {
         path: "/",
-        name: "Home",
+        name: "home",
         component: Home,
+    },
+    {
+        path: "/mystories",
+        name: "mystories",
+        component: Mystories,
+    },
+    {
+        path: "/werewolves",
+        name: "werewolves",
+        component: Werewolves,
+    },
+    {
+        path: "/chameleon",
+        name: "chameleon",
+        component: Chameleon,
+    },
+    {
+        path: "/half-a-minute",
+        name: "halfAMinute",
+        component: HalfAMinute,
     }
 ]
 
